05913c4f00e0103e89e61bd5794fa5f88b518809
John R. Estes <John.R.Estes@icloud.com>
2020-10-25 21:22:06 -0400

    updated bird image text and centering

--- src/components/BirdFacts/BirdFacts.js
+++ src/components/BirdFacts/BirdFacts.js
@@ -12,14 +12,19 @@ function Friends() {
 
           <div className="row">
             <div className="col-12">
-            <button onClick={() => dispatch(createBirdFact())}>Generate</button><br></br>
-              <h5>Random Bird Fact: {fact}</h5>
-              </div>
-              <div className="row">
-              <div className="col-12"> 
+            <button className="birdButton" onClick={() => dispatch(createBirdFact())}>Generate</button><br></br>
+              <h4>Random Bird Fact: </h4>
+              <br></br>
+              <h5>{fact}</h5>
+              <br></br>
+            </div>
+            <div className="container">
+            <div className="row">
+              <div className="span9 centred"> 
                 <h5>Random Bird Picture: </h5>
-                <img id="bird-img" alt="birdie" className="image-fluid"/><br></br>
-                <button onClick={() => dispatch(createBirdImage())}>Generate</button>
+                <img id="bird-img" alt="birdie"/><br></br>
+                <button className="birdButton" onClick={() => dispatch(createBirdImage())}>Generate</button>
+              </div>
               </div>
             </div>
           </div>
