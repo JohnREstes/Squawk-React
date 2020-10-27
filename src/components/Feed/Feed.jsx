649e0d5bd126fcd37e57b626e204c85c275ad537
John R. Estes <John.R.Estes@icloud.com>
2020-10-27 18:38:54 -0400

    updated feed to show image next to post

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -4,33 +4,40 @@ import { connect } from 'react-redux'
 
 class Feed extends React.Component {
 
-  createImage(){
-    const userImage = "data:image/png;base64," + this.props.base64TextString;
-    console.log(userImage);
-    const preview = document.getElementById('profile-pic');
-    preview.src = userImage;
-  }
+  // createImage(){
+  //   const userImage = "data:image/png;base64," + this.props.base64TextString;
+  //   console.log(userImage);
+  //   const preview = document.getElementById('profile-pic');
+  //   preview.src = userImage;
+  // }
 
-  componentDidMount(){
-    this.createImage();
-  }
+  // componentDidMount(){
+  //   this.createImage();
+  // }
+
+  // componentDidUpdate(){
+  //   this.createImage();
+  // }
 
   render(){
-  return (
-        <div className="col-6 text-center center">
+    return (!this.props.base64TextString ? 
+      <div className="col-6 text-center center">
+        <h1 id="loading">Loading...</h1>
+        </div> : (
+        <div className="col-6 center">
           <div className="row">
-          <div className="span9 centred"> 
-            <img id="profile-pic" alt="User uploaded for profile"/><br></br>
-          </div>
-            <div className="col-12">
+            <div className="col-12 justiy-content-center feed-div">
                 <h1>Feed:</h1>
                 {this.props.feed.feed.map(x => (
-                    <p key={x.id}>{x.title}</p>
+                    <p key={x.id}>
+                    {this.props.base64TextString ? <img class="profile-pic" src={`data:image/png;base64,${this.props.base64TextString}`}/>: ''}
+                      <br></br>
+                      {x.title}</p>
                 ))}
             </div>
           </div>
         </div>
-  );
+  ));
 }
 }
 
